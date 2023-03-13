42a0cd6799a7c5dd1a0bd74b33ce93b88e510b2d
RAM111111 <r.r.99@windowslive.com>
2023-03-13 21:36:07 +0300

    upd

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -7,6 +7,8 @@ export POSTGRES_DB=postgres
 export AWS_BUCKET=arn:aws:s3:::udagarm
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
+export PORT=8080
 export JWT_SECRET=ram-coding
 export URL=http://udagram-api-dev3.us-east-1.elasticbeanstalk.com
+
 
