14deef7a6a958c5322eee5a00618aeefa0f16a4b
RAM111111 <r.r.99@windowslive.com>
2023-03-13 04:54:04 +0300

    Done

---
+++ README.md
@@ -0,0 +1,9 @@
+# Hosting a Full Stack Application
+Hosting an applicatioon using aws
+
+
+
+# Hosting-a-Full-Stack-Application
+# Hosting-a-Full-Stack-Application
+# Hosting-a-Full-Stack-Application
+# Hosting-a-Full-Stack-Application
---
+++ udagram/set_env.sh
@@ -0,0 +1,12 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=aabb1122
+export POSTGRES_HOST=database-1.c7box8og2i2v.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=arn:aws:s3:::udagarm
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=ram-coding
+export URL=http://udagram-api-dev3.us-east-1.elasticbeanstalk.com
+
